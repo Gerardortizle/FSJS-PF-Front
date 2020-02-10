d9000bd2f22ad3e376983126169098dc38213f66
Gerardo <gerardortizle@gmail.com>
2020-02-10 15:08:43 -0600

    Moviendo a Folders

---
+++ Tareas/b03c14/b03c14ej1.js
@@ -0,0 +1,20 @@
+/*
+    Este ejercicio debe correr en Chrome.
+    Necesitarás ejercicio1.html.
+
+    Dentro de la función messageClick,
+    Agrega código para pedir su nombre al usuario,
+    Guarda ese nombre en una variable.
+    Después, despliega ese nombre en un saludo.
+*/
+
+function messageClick() {
+    //tu código va aquí vvv
+      const nombre = prompt("dame tu nombre", ); 
+    
+      alert (nombre);
+      
+    //tu código va aquí ^^^
+}
+
+//eof
---
+++ Tareas/b03c14/b03c14ej2.js
@@ -0,0 +1,58 @@
+/*
+    Este ejercicio debe correr en Node.
+
+    Usando variables de diferentes tipos,
+    Para cada compañero(a) de la clase,
+    Guarda su nombre, su edad y si usa Windows o no.
+
+    Después, imprime esos datos en pantalla.
+*/
+
+//tu código va aquí vvv
+    let nombre1 = "Gaby"
+    let edad1 = "30"
+    let Windows1 = false        
+
+    let nombre2 = "Mariana"
+    let edad2 = "25"
+    let Windows2 = true 
+
+    let nombre3 = "Antonio"
+    let edad3 = "30"
+    let Windows3 = true 
+
+    let nombre4 = "Pepe"
+    let edad4 = "26"
+    let Windows4 = false 
+
+    let nombre5 = "Axel"
+    let edad5 = "30"
+    let Windows5 = false 
+
+    console.log(`Compañer@ ${nombre1}`);
+    console.log(`Tiene ${edad1} años`);
+    console.log(`Usa Windows? ${Windows1}`);
+    console.log(`   `);
+
+    console.log(`Compañer@ ${nombre2}`);
+    console.log(`Tiene ${edad2} años`);
+    console.log(`Usa Windows? ${Windows2}`);
+    console.log(`   `);
+
+    console.log(`Compañer@ ${nombre3}`);
+    console.log(`Tiene ${edad3} años`);
+    console.log(`Usa Windows? ${Windows3}`);
+    console.log(`   `); 
+
+    console.log(`Compañer@ ${nombre4}`);
+    console.log(`Tiene ${edad4} años`);
+    console.log(`Usa Windows? ${Windows4}`);
+    console.log(`   `);
+
+    console.log(`Compañer@ ${nombre5}`);
+    console.log(`Tiene ${edad5} años`);
+    console.log(`Usa Windows? ${Windows5}`);
+
+//tu código va aquí ^^^
+
+//eof
