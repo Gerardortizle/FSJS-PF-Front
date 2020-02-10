aabc8e95e69c3da2379814d2495df0412ad280ac
Gerardo <gerardortizle@gmail.com>
2020-02-10 12:29:01 -0600

    Cambios Feb 10 2020

---
+++ b03c14ej2.js
@@ -0,0 +1,26 @@
+/*
+    Este ejercicio debe correr en Node.
+
+    Usando variables de diferentes tipos,
+    Para cada compañero(a) de la clase,
+    Guarda su nombre, su edad y si usa Windows o no.
+
+    Después, imprime esos datos en pantalla.
+*/
+
+//tu código va aquí vvv
+    let nombre1 = "Gaby"
+    let edad1 = "30"
+    let Windows = false        
+
+    console.log(`Compañera ${nombre1}`);
+    console.log(`Tiene estos años ${edad1}`);
+    console.log(`Usa Windows $);
+
+
+
+     
+
+//tu código va aquí ^^^
+
+//eof
